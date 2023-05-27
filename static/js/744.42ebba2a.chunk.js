b1bc37bdc219cd4acdc6144bd4f24a36df09ed86
Kcepriu <66989313+Kcepriu@users.noreply.github.com>
2023-05-27 18:46:45 +0000

    Deploying to gh-pages from @ Kcepriu/front_end_delivery@283126757ebb828736ef642fad001c01b3baa291 🚀

---
+++ static/js/744.42ebba2a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront_end_delivery=self.webpackChunkfront_end_delivery||[]).push([[744],{2744:function(e,n,r){r.r(n);var t=r(184);n.default=function(){return(0,t.jsx)("p",{children:"NotFound"})}}}]);
+//# sourceMappingURL=744.42ebba2a.chunk.js.map
